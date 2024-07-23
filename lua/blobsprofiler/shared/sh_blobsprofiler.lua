03787f216465301dc93d95233b77919bbb41ede8
blobles-dev <75809218+blobles-dev@users.noreply.github.com>
2024-07-23 23:35:11 +0100

    Ungodly log method with variable commands

--- lua/blobsprofiler/shared/sh_blobsprofiler.lua
+++ lua/blobsprofiler/shared/sh_blobsprofiler.lua
@@ -9,6 +9,77 @@ blobsProfiler.Restrictions.Concommand = blobsProfiler.Restrictions.Concommand or
 blobsProfiler.Client = blobsProfiler.Client or {}
 blobsProfiler.Server = blobsProfiler.Server or {}
 
+blobsProfiler.DebugMode = true -- This is currently only used by the logger
+
+blobsProfiler.L_NH_ERROR = -2 -- Throw error, no halt
+blobsProfiler.L_ERROR = -1 -- Throws error
+blobsProfiler.L_DEBUG = 0 -- For debug messages (can be hidden with DebugMode = false)
+blobsProfiler.L_LOG = 1 -- Prints, but also stores
+blobsProfiler.L_INFO = 2 -- Prints
+
+
+--[[
+        blobsProfiler.Log(<optional> number lLevel, string lMessage, <optional> string lSendToServer, <optional> bool lErrNHStack)
+        - Logs a lMessage with the lLevel type on lRealm(s)
+
+            <optional> number lLevel: The log level, available values: blobsProfiler.L_* (DEFAULT: blobsProfiler.L_INFO)
+            string lMessage: The message to actually log 
+            <optional> bool lSendToServer: Send log to server (DEFAULT: false) has no effect when used on server realm.
+            <optional> bool lErrStack: Set to true to provide the error stack, only works with error log types (DEFAULT: false)
+        
+        Examples: -- I think I covered every case?
+            blobsProfiler.Log("Test message")
+            blobsProfiler.Log("Test message", true)
+            blobsProfiler.Log("Test message", false, true)
+            blobsProfiler.Log(blobsProfiler.L_DEBUG, "Test message with debug")
+            blobsProfiler.Log(blobsProfiler.L_DEBUG, "Test message with debug to server", true)
+]]
+blobsProfiler.Log = function(lLevel, lMessage, lSendToServer, lErrStack) -- TODO: send to server
+    -- The shit we're about to do for variable arguments..
+    if type(lLevel) == "string" and type(lMessage) == "boolean" then -- blobsProfiler.Log(lMessage, lSendToServer) / blobsProfiler.Log(lMessage, lSendToServer, lErrStack)
+        lSendToServer = lMessage
+        lMessage = lLevel
+        lLevel = blobsProfiler.L_INFO
+        lErrStack = lSendToServer or false
+    elseif type(lLevel) == "string" then -- blobsProfiler.Log(lMessage)
+        lMessage = lLevel
+        lLevel = blobsProfiler.L_INFO
+        lSendToServer = false
+        lErrStack = false
+    elseif type(lLevel) == "number" then -- Standard parameters
+        if type(lMessage) ~= "string" then
+            error("\nblobsProfiler.Log: Invalid parameters")
+        end
+        lSendToServer = lSendToServer or false
+        lErrStack = lErrStack or false
+    else
+        error("\nblobsProfiler.Log: Invalid parameters")
+    end
+
+    local curTime = os.date("%H:%M:%S") -- tHaNK gOd lUa is caSe sEnSitiVE
+    if lLevel == blobsProfiler.L_NH_ERROR then
+        if lErrStack then
+            ErrorNoHaltWithStack(string.format("[%s] [ERROR_S] blobsProfiler: %s", curTime, lMessage), 2)
+        else
+            ErrorNoHalt(string.format("[%s] [ERROR] blobsProfiler: %s\n", curTime, lMessage))
+        end
+    elseif lLevel == blobsProfiler.L_ERROR then
+        if lErrStack then
+            error(string.format("\n[%s] [CRITICAL_S] blobsProfiler: %s", curTime, lMessage), 2)
+        else
+            Error(string.format("[%s] [CRITICAL] blobsProfiler: %s\n", curTime, lMessage))
+        end
+    elseif lLevel == blobsProfiler.L_DEBUG and blobsProfiler.DebugMode then
+        print(string.format("[%s] [DEBUG] blobsProfiler: %s", curTime, lMessage))
+    elseif lLevel == blobsProfiler.L_LOG then
+        print(string.format("[%s] [LOG] blobsProfiler: %s", curTime, lMessage))
+        -- TODO: Store it
+    else -- Default / Generic
+        print(string.format("[%s] [INFO] blobsProfiler: %s", curTime, lMessage))
+    end
+end
+
+
 blobsProfiler.InitSQLiteData = function()
     local rootDataTable = SERVER and blobsProfiler.Server or blobsProfiler.Client
     
